6833b051a8c88869f88a22ec0270d263f8a160be
Azar <azar.wirth@outlook.com>
2025-02-20 10:21:39 +0100

    added baking-a-website folder

---
+++ baking_a_website/baking_a_website.js
@@ -0,0 +1,11 @@
+//open sidepanel
+function openNav() {
+    const sidepanel = document.getElementById("mySidepanel");
+    sidepanel.classList.add("open");
+}
+
+//close sidepanel
+function closeNav() {
+    const sidepanel = document.getElementById("mySidepanel");
+    sidepanel.classList.remove("open");
+}
